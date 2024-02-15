b2999675534343fef7bc338d9388c301525db22d
Alberto <lzacarias@sinprol.com.mx>
2024-02-15 15:32:08 -0600

    initial commit

---
+++ src/main.js
@@ -0,0 +1,38 @@
+// import Vue from 'vue'
+// import App from './App.vue'
+// import VueRouter from 'vue-router'
+// import {routes} from './routes'
+// import {store} from './store/store'
+
+// Vue.use(VueRouter)
+
+// const router = new VueRouter({
+//   routes,
+//   mode:'history'
+// })
+
+// Vue.config.productionTip = false
+
+// new Vue({
+//   render: h => h(App),
+//   router,
+//   store
+// }).$mount('#app')
+
+
+//Actualizando a vuejs3
+import App from './App.vue'
+import {createApp} from 'vue'
+import {createRouter, createWebHistory} from 'vue-router'
+import {routes} from './routes'
+import {store} from './store/store'
+
+const router = createRouter({
+    routes,
+    history:createWebHistory()
+})
+
+const app = createApp(App)
+app.use(router)
+app.use(store)
+app.mount('#app')
---
+++ src/store/store.js
@@ -0,0 +1,47 @@
+
+
+// import Vue from 'vue'
+// import Vuex from 'vuex'
+// import moduloCampeonato from './modulos/campeonato'
+// import * as getters from './getters'
+// import * as mutations from './mutations'
+// import * as actions from './actions'
+
+// Vue.use(Vuex)
+
+
+// export const store = new Vuex.Store({
+//   state:{
+//     valor: 10,
+//     nombre: 'Federico',
+//     comentarios:"Comentarios desde el store"
+//   },
+//   getters,
+//   mutations,
+//   actions,
+//   modules:{
+//       moduloCampeonato
+//   }
+// })
+
+
+//Actualizando a vuejs3
+import {createStore} from 'vuex'
+import moduloCampeonato from './modulos/campeonato'
+import * as getters from './getters'
+import * as mutations from './mutations'
+import * as actions from './actions'
+
+export const store = createStore({
+    state:{
+      valor: 10,
+      nombre: 'Federico',
+      comentarios:"Comentarios desde el store"
+    },
+    getters,
+    mutations,
+    actions,
+    modules:{
+        moduloCampeonato
+    }
+  })
